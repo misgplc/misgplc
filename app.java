7c98f0d376d80c4108144c09aff71c9028727990
Misgana G <misganastudy@gmail.com>
2023-04-04 19:53:02 -0400

    May be is correct

--- app.java
+++ app.java
@@ -1,2 +1,12 @@
 This Is A Great.
 Developing Java App
+On branch master
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	aa.java
+
+nothing added to commit but untracked files present (use "git add" to track)
+-a -m .net  Apps
+.net  Apps
+.net Apps
+.Net Application
